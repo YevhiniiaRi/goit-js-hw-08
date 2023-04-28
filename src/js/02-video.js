4c5aee123f8a1a635c34ef9c010e43c5958ab87d
YevheniiaRi <9263070@gmail.com>
2023-04-28 17:15:37 +0300

    HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
 
+const player = new Player('video-player', {
+  id: 236203659,
+  autoplay: true,
+});
+
+import throttle from 'lodash.throttle';
+
+const saveCurrentTime = currentTime => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(currentTime => {
+      saveCurrentTime(currentTime);
+    });
+  }, 1000)
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
+
+import throttle from 'lodash.throttle';
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+  }, 1000)
+);
