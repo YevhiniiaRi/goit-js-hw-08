064375f0f399a28c0bf9ca1ad4d80e154aa29fe5
YevheniiaRi <9263070@gmail.com>
2023-04-28 18:18:28 +0300

    HW

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,11 @@
 import throttle from 'lodash.throttle';
 
-// Get the form and its elements
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageInput = feedbackForm.querySelector('textarea[name="message"]');
 
-// Set the key for the localStorage object
 const storageKey = 'feedback-form-state';
 
-// Function to update the localStorage object
 const updateStorage = throttle(() => {
   const state = {
     email: emailInput.value,
@@ -17,11 +14,10 @@ const updateStorage = throttle(() => {
   localStorage.setItem(storageKey, JSON.stringify(state));
 }, 500);
 
-// Add input event listeners to the email and message inputs
+
 emailInput.addEventListener('input', updateStorage);
 messageInput.addEventListener('input', updateStorage);
 
-// Function to load the form state from the localStorage object
 const loadFormState = () => {
   const state = JSON.parse(localStorage.getItem(storageKey));
   if (state) {
@@ -30,28 +26,22 @@ const loadFormState = () => {
   }
 };
 
-// Load the form state on page load
 loadFormState();
 
-// Function to handle form submission
 const handleSubmit = event => {
   // Prevent the default form submission behavior
   event.preventDefault();
 
-  // Get the current form state
   const state = {
     email: emailInput.value,
     message: messageInput.value,
   };
 
-  // Clear the localStorage object and form fields
   localStorage.removeItem(storageKey);
   emailInput.value = '';
   messageInput.value = '';
 
-  // Log the form state to the console
   console.log(state);
 };
 
-// Add submit event listener to the form
 feedbackForm.addEventListener('submit', handleSubmit);
